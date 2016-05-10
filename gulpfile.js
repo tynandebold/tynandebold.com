e7ce9062d3bcfa262f32db4d3c15eedd55f5e935
Tynan DeBold <thdebold@gmail.com>
2016-05-10 15:44:30 -0400

    first commit; everything up and running

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// include gulp
+var gulp = require('gulp');
+
+// include plugins
+var nunjucks   = require('gulp-nunjucks-render');
+var data       = require('gulp-data');
+var compass    = require('gulp-compass');
+var livereload = require('gulp-livereload');
+var webserver  = require('gulp-webserver');
+var del        = require('del');
+
+//default tasks
+gulp.task('default', ['compass', 'nunjucks', 'watch', 'webserver']);
+gulp.task('build', ['nunjucks', 'build']);
+
+gulp.task('nunjucks', function(){
+  return gulp.src('./app/pages/**/*.nunjucks')
+    .pipe(data(function() {
+      var data = require('./app/data/context.json');
+      return data;
+    }))
+    .pipe(nunjucks({
+      path: ['./app/templates']
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+// scss to css
+gulp.task('compass', function() {
+  return gulp.src('./app/scss/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: './app/css',
+      sass: './app/scss'
+    }));
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./app/scss/*.scss')
+    .pipe(sass({compass: true}))
+    .pipe(gulp.dest('./app/css'));
+});
+
+// watch files for changes
+gulp.task('watch', function() {
+	livereload.listen()
+    gulp.watch('./app/scss/*.scss', ['compass'])
+    gulp.watch('./app/**/**/*.+(nunjucks|json)', ['nunjucks']);
+});
+
+// run a local server
+gulp.task('webserver', function(){
+	return gulp.src('./app/')
+		.pipe(webserver({
+			open: true
+		}));
+});
+
+// organize and build only necessary files
+gulp.task('build', function(){
+  del('./build/**');
+  return gulp.src(['./app/assets/**', './app/css/**', './app/*.html'],{ base: 'app' })
+    .pipe(gulp.dest('./build'));
+});
