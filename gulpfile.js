706361415197b5deaf0206e84504e5b20da811e0
Tynan DeBold <thdebold@gmail.com>
2019-05-24 11:41:23 +0200

    update gulp task to rename css file; added mobile addiciton test project; update some deps; update readme

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,11 @@ var livereload = require('gulp-livereload');
 var nunjucks = require('gulp-nunjucks-render');
 var runSeq = require('run-sequence');
 var webserver = require('gulp-webserver');
+var rename = require('gulp-rename');
 
 // specific tasks
 gulp.task('default', function() {
-  runSeq('compass', 'nunjucks', 'watch', 'webserver', 'minify-css');
+  runSeq('compass', 'nunjucks', 'watch', 'webserver');
 });
 
 gulp.task('build', function() {
@@ -22,68 +23,83 @@ gulp.task('build', function() {
 
 // get data; run nunjucks to compile static html files
 gulp.task('nunjucks', function() {
-  return gulp.src('./app/pages/**/*.nunjucks')
-    .pipe(data(function() {
-      var data = require('./app/data/context.json');
-      return data;
-    }))
-    .pipe(nunjucks({
-      path: ['./app/templates']
-    }))
+  return gulp
+    .src('./app/pages/**/*.nunjucks')
+    .pipe(
+      data(function() {
+        var data = require('./app/data/context.json');
+        return data;
+      })
+    )
+    .pipe(
+      nunjucks({
+        path: ['./app/templates']
+      })
+    )
     .pipe(gulp.dest('./app'));
 });
 
 // config scss/css with compass
 gulp.task('compass', function() {
-  return gulp.src('./app/scss/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: './app/css',
-      sass: './app/scss'
-    }));
-});
-
-// compile scss
-gulp.task('sass', function() {
-  return gulp.src('./app/scss/*.scss')
-    .pipe(sass({ compass: true }))
+  return gulp
+    .src('./app/scss/*.scss')
+    .pipe(
+      compass({
+        config_file: './config.rb',
+        css: './app/css',
+        sass: './app/scss'
+      })
+    )
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./app/css'));
 });
 
 // minify css
 gulp.task('minify-css', function() {
-  return gulp.src('./app/css/*.css')
+  return gulp
+    .src('./app/css/*.min.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('./app/css'));
 });
 
 // watch files for changes
 gulp.task('watch', function() {
-  livereload.listen()
-  gulp.watch('./app/scss/*.scss', ['compass'])
-  gulp.watch('./app/css/*.css', ['minify-css'])
+  livereload.listen();
+  gulp.watch('./app/scss/*.scss', ['compass']);
+  gulp.watch('./app/css/*.min.css', ['minify-css']);
   gulp.watch('./app/**/**/*.+(nunjucks|json)', ['nunjucks']);
 });
 
 // run a local server
 gulp.task('webserver', function() {
-  return gulp.src('./app/')
-    .pipe(webserver({
+  return gulp.src('./app/').pipe(
+    webserver({
       open: true
-    }));
+    })
+  );
 });
 
 // image optimization
 gulp.task('responsive', function() {
   del('./build/**/*');
-  return gulp.src('./app/assets/photo/**/*.jpg')
+  return gulp
+    .src('./app/assets/photo/**/*.jpg')
     .pipe(imagemin({ progressive: true }))
     .pipe(gulp.dest('./build/assets/photo'));
 });
 
 // move necessary files to build dir
 gulp.task('move', function() {
-  return gulp.src(['./app/assets/dev/**/*', './app/css/**/*', './app/*.html', './app/js/*.js'], { base: 'app' })
+  return gulp
+    .src(
+      [
+        './app/assets/dev/**/*',
+        './app/css/*.min.css',
+        './app/*.html',
+        './app/js/*.js'
+      ],
+      { base: 'app' }
+    )
     .pipe(gulp.dest('./build'));
 });
 
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
-
 ### Setup
+
 ```
+$ sudo gem install -n /usr/local/bin compass
 $ npm install
 ```
 
 ### Run
+
 ```
 $ npm start
 ```
